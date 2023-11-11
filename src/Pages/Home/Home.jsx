f50c70fd97636e6189baf6444cc9568824270259
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-12 00:20:51 +0600

    popular item section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Category from "../Category/Category";
 import Banner from "../Shared/Banner/Banner";
+import PopularMenu from "./PopularMenu";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Category></Category>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
