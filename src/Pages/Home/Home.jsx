aa8c9f2f0976a1157bf9e622328a43e7f5f04d13
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-11 15:41:03 +0600

    swiper added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Category from "../Category/Category";
 import Banner from "../Shared/Banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
