91de25a4089c1d6ebc6b3cb8b0567a7cd6e9bfeb
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-14 11:22:24 +0600

    database added

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -6,11 +6,11 @@ const FoodCard = ({item}) => {
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={image} alt="Shoes" /></figure>
             <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4">${price}</p>
-            <div className="card-body">
-                <h2 className="card-title text-center">{name}</h2>
+            <div className="card-body  flex flex-col items-center">
+                <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-outline border-0 bg-slate-100 border-b-4 mt-4 uppercase border-orange-400">Add to cart</button>
                 </div>
             </div>
         </div>
