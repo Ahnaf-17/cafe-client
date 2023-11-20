079749b9028c75c843e1b74072e8b02787adc53c
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-20 11:26:08 +0600

    token varified

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -2,6 +2,7 @@
 import { createContext, useEffect, useState } from "react";
 import { app } from "../Firebase/firebase.config";
 import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from 'firebase/auth'
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
 const AuthProvider = ({children}) => {
@@ -9,6 +10,7 @@ const AuthProvider = ({children}) => {
     const [user,setUser] = useState(null)
     const [loading,setLoading] = useState(true)
     const provider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic()
 
 
     const createUser = (email,password) =>{
@@ -44,9 +46,17 @@ const AuthProvider = ({children}) => {
             setUser(currentUser)
             if(currentUser){
                 // get token and store client 
+                const userInfo = {email: currentUser.email};
+                axiosPublic.post('/jwt',userInfo)
+                .then(res=>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token',res.data.token)
+                    }
+                })
             }
             else{
                 // remove token(if token stored in the client side: local storage, caching,in memory)
+                localStorage.removeItem('access-token')
             }
             setLoading(false) 
         })
