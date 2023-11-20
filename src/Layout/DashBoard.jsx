1c1b846941b5ce608836fb130a9c009b324a2774
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-20 12:13:25 +0600

    make admin

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -2,10 +2,11 @@
 import { FaAd, FaBook, FaCalendar, FaComment, FaHome, FaList, FaMobileAlt, FaSearch, FaShoppingCart, FaUser, FaUsers, FaUtensils } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../Hooks/useCart";
+import useAdmin from "../Hooks/useAdmin";
 
 const DashBoard = () => {
     const [cart] = useCart();
-    const isAdmin = true
+    const [isAdmin] = useAdmin()
     return (
         <div className="flex">
             <div className="w-64 min-h-screen bg-orange-400">
