3d1132bb182b4c2d3c35158916e8211be4ef6e8d
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-20 15:33:19 +0600

    user api secure using verify Admin

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../Hooks/useAdmin";
+import useAuth from "../Hooks/useAuth";
+
+const AdminRoute = ({children}) => {
+    const [isAdmin,isAdminLoading] = useAdmin()
+    const {user,loading} = useAuth()
+    const location = useLocation()
+    if(loading || isAdminLoading){
+        return <span className="loading loading-dots loading-lg"></span>
+    }
+    if(user && isAdmin){
+        return children;
+    }
+    // return <Navigate state={location.pathname} to='/login'></Navigate>
+    return <Navigate state={{from: location}} replace to='/login'></Navigate>
+}
+
+
+export default AdminRoute;
